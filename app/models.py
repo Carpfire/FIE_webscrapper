c8fd9e38a5e97303c69c397a302c65ce01d22cd2
Carpfire <liamcarp22@gmail.com>
2022-06-01 13:34:42 -0400

    First Commit

---
+++ app/models.py
@@ -0,0 +1,66 @@
+from sqlalchemy import Table, Column, Integer, ForeignKey, String, Date
+from sqlalchemy.orm import relationship
+from .database import Base
+
+
+fencers = Table(
+    'fencers_bouts',
+    Base.metadata,
+    Column('fencers_id', Integer, ForeignKey('fencer.id')), 
+    Column('bout_id', Integer, ForeignKey('bout.id')) 
+)
+
+tourn_fen = Table(
+    'fencers_tournament',
+    Base.metadata, 
+    Column('fencer_id', Integer, ForeignKey('fencer.id')),
+    Column('tournament_id', Integer, ForeignKey('tournament.id'))
+)
+
+
+
+class Fencer(Base):
+
+    __tablename__ = 'fencer'
+    id = Column(Integer, primary_key = True)
+    first_name = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=False)
+    country = Column(String(255))
+
+
+
+class Tournament(Base):
+    
+    __tablename__ = 'tournament'
+    id = Column(Integer, primary_key=True)
+    country = Column(String(255))
+    city = Column(String(255))
+    date = Column(Date)
+    bouts = relationship('Bout', backref='tournament', lazy='select')
+    fencers = relationship('Fencer', secondary=tourn_fen, backref='tournament', lazy='select')
+
+
+
+class Bout(Base):
+
+    __tablename__ = 'bout'
+    id = Column(Integer, primary_key = True)
+    tournament_id = Column(Integer, ForeignKey('tournament.id'))
+    fencer_1_score = Column(Integer)
+    fencer_2_score = Column(Integer)
+    rnd = Column(Integer)
+    fencers = relationship('Fencer', secondary=fencers, backref='bouts', lazy='select')
+
+
+
+#Will implement after web scrapping -> database is working
+
+class Touch(Base):
+    
+    __tablename__ = 'touch'
+    bout_id = Column(Integer, ForeignKey('bout.id'))
+    id = Column(Integer, primary_key=True)
+    point = Column(Integer)
+    score = Column(Integer)
+
+
